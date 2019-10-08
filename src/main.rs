75611ffd8c63ac47eb9c18105fc88ae2083a1f07
aQaTL <mmsoltys@outlook.com>
2019-10-08 13:22:31 +0000

    Added config

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use actix::{Actor, Addr, System};
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use actix_web_actors::ws;
 use std::io;
+use serde::Deserialize;
 
 mod websocket_server;
 
 use websocket_server::*;
 
+#[derive(Deserialize)]
+struct Config {
+	ip: String,
+	port: u16,
+}
+
 fn main() -> io::Result<()> {
+	let config = {
+		let data = std::fs::read("config.toml")?;
+		toml::from_slice::<Config>(data.as_slice())?
+	};
+
 	let sys = System::new(env!("CARGO_PKG_NAME"));
 
 	let ws_server_actor = WebsocketServer {}.start();
@@ -18,7 +30,7 @@ fn main() -> io::Result<()> {
 			.route("/ws", web::get().to(websocket_connect))
 			.service(actix_files::Files::new("/", "/frontend"))
 	})
-	.bind(":80")?
+	.bind(format!("{}:{}", config.ip, config.port))?
 	.start();
 
 	sys.run()
